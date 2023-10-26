3281949d04c3424ac935b36af05747d699e399ff
Slaaayer420 <129106428+Slaaayer420@users.noreply.github.com>
2023-10-26 01:30:51 -0400

    test

--- app/src/main/java/com/example/servicenovigrad/MainActivity.java
+++ app/src/main/java/com/example/servicenovigrad/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.servicenovigrad.ServicesNovigradClass.Client;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonSignIn;
@@ -31,13 +34,20 @@ public class MainActivity extends AppCompatActivity {
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openactivity_accueil();
+                EditText username = (EditText) findViewById(R.id.editUsername);
+                EditText password = (EditText) findViewById(R.id.editPassword);
+                Client client = new Client(username.getText().toString(),password.getText().toString());
+                openactivity_login();
             }
         });
     }
 
     public void openactivity_accueil() {
         Intent intent = new Intent(this, AccueilActivity.class);
+        startActivity(intent);
+    }
+    public void openactivity_login() {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
